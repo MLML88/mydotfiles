5cc53905313044e697a62ac9008ef19beee233c9
MLML8 <jacky55668884@gmail.com>
2026-02-01 13:24:18 -0500

    Adds theme to fcitx5

--- exec/chinese_install.sh
+++ exec/chinese_install.sh
@@ -5,3 +5,12 @@ yay -S noto-fonts-cjk noto-fonts-emoji --needed
 yay -S adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts --needed
 
 yay -S fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool fcitx5-chinese-addons --needed
+
+if [ ! -d "$HOME/fcitx5/" ]; then
+    git clone https://github.com/catppuccin/fcitx5.git ~
+    mkdir -p ~/.local/share/fcitx5/themes/
+    cp -r ~/fcitx5/src/* ~/.local/share/fcitx5/themes
+    echo "In ~/.config/fcitx5/conf/classicui.conf add Theme=catppuccin-mocha-blue"
+else
+    echo "fcitx5 theme already exist"
+fi
