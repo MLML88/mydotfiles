8568ea9e8960cbb8803785a9b50b5a640ba15ca5
MLML8 <jacky55668884@gmail.com>
2026-05-13 17:49:48 -0400

    update

--- exec/asus_install.sh
+++ exec/asus_install.sh
@@ -1,6 +1,6 @@
 # Update packages
 yay -Sy
 
-yay -S --needed asusctl supergfxctl rog-control-center
+yay -S --needed asusctl supergfxctl rog-control-center nvidia-580xx-dkms nvidia-580xx-utils
 
 sudo systemctl enable --now asusd
