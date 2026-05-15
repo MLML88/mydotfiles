d41486e6705a8fa18d6fc203949e31e3785c4e53
Jacky Lin <jacky55668884@gmail.com>
2026-05-15 14:47:07 -0400

    Reformating

--- nvim/.config/nvim/lua/config/keybinds.lua
+++
@@ -1,9 +0,0 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = ","
-
-local set = vim.keymap.set
-
-set("n", "<leader>cd", vim.cmd.Ex)
-
-set('v', 'J', ":m '>+1<CR>gv=gv")
-set('v', 'K', ":m '<-2<CR>gv=gv")
