f7470bef4450a24fba277bad936df7ff5192b526
Jacky Lin <jacky55668884@gmail.com>
2026-06-08 00:31:36 -0400

    Back on cachyos and made some changes

--- MLML/.config/fish/config.fish
+++ MLML/.config/fish/config.fish
@@ -1,4 +1,4 @@
-# source /usr/share/cachyos-fish-config/cachyos-config.fish
+source /usr/share/cachyos-fish-config/cachyos-config.fish
 
 if status is-interactive
 # Commands to run in interactive sessions can go here
